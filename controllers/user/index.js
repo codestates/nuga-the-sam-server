864808b45c7116370e90becfad919360ec8fb2f6
Jisang Park <qkrwltkd99@gmail.com>
2021-02-05 16:14:09 +0900

    completed #11 (sign up)

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -1,6 +1,7 @@
 const { user } = require("../../models");
 const crypto = require("crypto-js");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 module.exports = {
 	//특정 유저데이터 요청(마이페이지)
@@ -11,7 +12,30 @@ module.exports = {
 	//회원가입
 	//POST /users/signup
 	sign_up: async (req, res) => {
-		res.send();
+		const { nickname, email, password } = req.body;
+		if (nickname && email && password) {
+			const conflictedEmail = await user.findOne({ where: { email } });
+			const conflictedNick = await user.findOne({ where: { nickname } });
+			console.log(conflictedEmail);
+			if (!conflictedEmail && !conflictedNick) {
+				const hashedPassword = crypto.PBKDF2(password, process.env.SALT, {
+					keySize: 512 / 32,
+					iterations: 123432,
+				});
+				await user.create({
+					nickname,
+					email,
+					password: hashedPassword.toString(),
+				});
+				res.status(200).end();
+			} else {
+				res
+					.status(409)
+					.json({ message: "This email or nickname already exists" });
+			}
+		} else {
+			res.status(400).json({ message: "invalid input" });
+		}
 	},
 	//로그인
 	//POST /users/login
