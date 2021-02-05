724306d1f41c7127547278e5754a0d92f65d0c8b
=chiemsee30 <=jetignis@gmail.com>
2021-02-05 17:21:48 +0900

    completed feature/#10

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -20,7 +20,7 @@ module.exports = {
 			if (!conflictedEmail && !conflictedNick) {
 				const hashedPassword = crypto.PBKDF2(password, process.env.SALT, {
 					keySize: 512 / 32,
-					iterations: 123432,
+					iterations: 32450,
 				});
 				await user.create({
 					nickname,
@@ -40,11 +40,60 @@ module.exports = {
 	//로그인
 	//POST /users/login
 	log_in: async (req, res) => {
-		res.send();
+		//email: req.body.email
+		//password: req.body.password
+		const { email, password } = req.body;
+		if (email && password) {
+			const hashedPassword = crypto.PBKDF2(password, process.env.SALT, {
+				keySize: 512 / 32,
+				iterations: 32450,
+			});
+			const result = await user.findOne({
+				where: { email, password: hashedPassword.toString() },
+			});
+			if (result) {
+				const token = jwt.sign(
+					{
+						id: result.id,
+						nickname: result.nickname,
+						email: result.email,
+					},
+					process.env.ACCESS_SECRET,
+					{
+						expiresIn: "1h",
+					},
+				);
+				res.json({ token });
+			} else {
+				res.status(404).json({ message: "not found" });
+			}
+		} else {
+			res.status(400).json({ message: "invalid input" });
+		}
 	},
 	//로그아웃
 	//POST /users/logout
 	log_out: async (req, res) => {
+		res.send();
+	},
+	//소셜로그인
+	//POST /users/sociallogin
+	social_login: async (req, res) => {
+		res.send();
+	},
+	//소셜 회원가입
+	//POST /users/socialsignup
+	social_signup: async (req, res) => {
+		res.send();
+	},
+	//이메일 중복확인
+	//POST /users/signup/checkemail
+	check_email: async (req, res) => {
+		res.send();
+	},
+	//닉네임 중복황인
+	//POST /users/signup/checknick
+	check_nick: async (req, res) => {
 		res.send();
 	},
 };
