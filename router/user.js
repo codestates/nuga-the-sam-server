d15daf8a278a5b0d6c648557d9b2ad690cb36cc4
Jisang Park <qkrwltkd99@gmail.com>
2021-02-08 14:13:41 +0900

    modified router url

--- router/user.js
+++ router/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const user_controller = require("../controllers/user");
 
-router.get("/:id", user_controller.get_user_data);
+router.get("/", user_controller.get_user_data);
 router.post("/signup", user_controller.sign_up);
 router.post("/signup/checkemail", user_controller.check_email);
 router.post("/signup/checknick", user_controller.check_nick);
