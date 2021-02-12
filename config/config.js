05b7ea1e0f2145fddd67455d0212462ee490f6dd
Jisang Park <qkrwltkd99@gmail.com>
2021-02-12 19:03:57 +0900

    modified reading timestamp set

--- config/config.js
+++ config/config.js
@@ -7,56 +7,29 @@ module.exports = {
 		database: "nuga-the-sam",
 		host: process.env.DB_HOST,
 		dialect: "mysql",
+		dialectOptions: {
+			useUTC: false, // for reading from database
+		},
 		timezone: "+09:00",
-		pool: {
-      			max: 5,
-      			min: 0,
-      			acquire: 30000,
-      			idle: 10000,
-    		},
-    		define: {
-      			// The `timestamps` field specify whether or not the `createdAt` and `updatedAt` fields will be created.
-      			// This was true by default, but now is false by default
-      			timestamps: false,
-      			supportBigNumbers: true,
-    		},
 	},
 	test: {
 		username: process.env.DB_USERNAME,
 		password: process.env.DB_PASSWORD,
 		database: "nuga-the-sam",
 		host: process.env.DB_HOST,
+		dialectOptions: {
+			useUTC: false, // for reading from database
+		},
 		timezone: "+09:00",
-		pool: {
-                        max: 5,
-                        min: 0,
-                        acquire: 30000,
-                        idle: 10000,
-                },
-                define: {
-                        // The `timestamps` field specify whether or not the `createdAt` and `updatedAt` fields will be created.
-                        // This was true by default, but now is false by default
-                        timestamps: false,
-                        supportBigNumbers: true,
-                },
 	},
 	production: {
 		username: process.env.DB_USERNAME,
 		password: process.env.DB_PASSWORD,
 		database: "nuga-the-sam",
 		host: process.env.DB_HOST,
-                timezone: "+09:00",
-                pool: {
-                        max: 5,
-                        min: 0,
-                        acquire: 30000,
-                        idle: 10000,
-                },
-                define: {
-                        // The `timestamps` field specify whether or not the `createdAt` and `updatedAt` fields will be created.
-                        // This was true by default, but now is false by default
-                        timestamps: false,
-                        supportBigNumbers: true,
-                },
+		dialectOptions: {
+			useUTC: false, // for reading from database
+		},
+		timezone: "+09:00",
 	},
 };
