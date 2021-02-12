d8ee7c3777326b21c365000ebafda191b933a87a
Ubuntu <ubuntu@ip-172-31-87-69.ec2.internal>
2021-02-12 19:01:20 +0900

    modified time stamp set

--- config/config.js
+++ config/config.js
@@ -6,18 +6,57 @@ module.exports = {
 		password: process.env.DB_PASSWORD,
 		database: "nuga-the-sam",
 		host: process.env.DB_HOST,
-		dialect: "mysql",		
+		dialect: "mysql",
+		timezone: "+09:00",
+		pool: {
+      			max: 5,
+      			min: 0,
+      			acquire: 30000,
+      			idle: 10000,
+    		},
+    		define: {
+      			// The `timestamps` field specify whether or not the `createdAt` and `updatedAt` fields will be created.
+      			// This was true by default, but now is false by default
+      			timestamps: false,
+      			supportBigNumbers: true,
+    		},
 	},
 	test: {
 		username: process.env.DB_USERNAME,
 		password: process.env.DB_PASSWORD,
 		database: "nuga-the-sam",
 		host: process.env.DB_HOST,
+		timezone: "+09:00",
+		pool: {
+                        max: 5,
+                        min: 0,
+                        acquire: 30000,
+                        idle: 10000,
+                },
+                define: {
+                        // The `timestamps` field specify whether or not the `createdAt` and `updatedAt` fields will be created.
+                        // This was true by default, but now is false by default
+                        timestamps: false,
+                        supportBigNumbers: true,
+                },
 	},
 	production: {
 		username: process.env.DB_USERNAME,
 		password: process.env.DB_PASSWORD,
 		database: "nuga-the-sam",
 		host: process.env.DB_HOST,
+                timezone: "+09:00",
+                pool: {
+                        max: 5,
+                        min: 0,
+                        acquire: 30000,
+                        idle: 10000,
+                },
+                define: {
+                        // The `timestamps` field specify whether or not the `createdAt` and `updatedAt` fields will be created.
+                        // This was true by default, but now is false by default
+                        timestamps: false,
+                        supportBigNumbers: true,
+                },
 	},
 };
